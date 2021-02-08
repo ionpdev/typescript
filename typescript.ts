f18c5ecfcedebbe661e03a7ef5db0f9e24debbcb
johny2433 <johny.codmw3@gmail.com>
2021-02-08 14:40:17 +0200

    TS Generics

--- typescript.ts
+++ typescript.ts
@@ -321,3 +321,79 @@ car2.startDrivingProcess()
 // protected
 
 // Fields with Inheritance
+
+
+// 
+// Generics
+// 
+class ArrayOfNumbers {
+  constructor(public collection: number[]) {}
+
+  get(index: number): number {
+    return this.collection[index]
+  }
+}
+
+class ArrayOfStrings {
+  constructor(public collection: string[]) {}
+
+  get(index: number): string {
+    return this.collection[index]
+  }
+}
+
+class ArrayOfAnything<T> {
+  constructor(public collection: T[]) {}
+
+  get(index: number): T {
+    return this.collection[index]
+  }
+}
+
+new ArrayOfAnything<string>(['a','b','c'])
+
+// Example of Generics with functions
+function printString(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+  }
+}
+  printAnything<string>(['a', 'b', 'c'])
+
+// Generic Constraints
+class Cars {
+  print() {
+    console.log('I am a car')
+  }
+}
+
+class House {
+  print() {
+    console.log('I am a house')
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print()
+  }
+}
+
+printHousesOrCars<House>([new House(), new House()])
+printHousesOrCars<Cars>([new Cars()])
